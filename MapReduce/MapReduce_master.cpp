85f3d82375faa7d1644e795273d35786dcf23867
Cody <zju21860025@gmail.com>
2024-07-17 22:39:52 +0800

    some basic functions

--- MapReduce/MapReduce_master.cpp
+++ MapReduce/MapReduce_master.cpp
@@ -2,6 +2,8 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <mutex>
+#include <unordered_map>
 #include <grpc++/grpc++.h>
 
 #include "MapReduce.grpc.pb.h"
@@ -21,19 +23,44 @@ using mapReduce::SetReduceTaskStatRequest;
 class MasterImpl final : public MapReduce::Service
 {
 public:
-    Status AssignMapTask(ServerContext* context, MapTaskReply* reply) override {}
+    Status AssignMapTask(ServerContext* context, MapTaskReply* reply) override
+    {
+        if (finishedMapTasks.size() == fileNum) {
+            reply->set_MapFileName("empty");
+        }
+        if (!fileList.empty()) {
+            std::lock_guard<std::mutex> lck(mtx); 
+            std::string task = fileList.back();
+            fileList.pop_back();
+            mtx.unlock();
+            AddTaskToRunningMapTasks(task);
+            return std::string(task);
+        }
+
+        return "empty";
+
+    }
     Status AssignReduceTask(ServerContext* context, MapTaskReply* reply) override {}
     Status SetMapTaskStat(ServerContext* context, SetMapTaskStatRequest* request) override {}
     Status SetReduceTaskStat(ServerContext* context, SetMapTaskStatRequest* request) override {}
 
-    void GetAllFiles(int argc, char* argv[]) {}
+    void GetAllFiles(int argc, char* argv[])
+    {
+        for (int i = 1; i < argc; i++) {
+            fileList.emplace_back(argv[i]);
+        }
+        fileNum = argc - 1;
+    }
+
+    void AddTaskToRunningMapTasks(const std::string& task) {}
 private:
     int fileNum;
     std::vector<std::string> fileList;
-    std::vector<std::string> runningMapTask;
-    std::vector<int> runningReduceTask;
-    std::unordered_map<std::tring, int> finishedMapTask;
-    std::unordered_map<int, int> finishedReduceTask;
+    std::vector<std::string> runningMapTasks;
+    std::vector<int> runningReduceTasks;
+    std::unordered_map<std::tring, int> finishedMapTasks;
+    std::unordered_map<int, int> finishedReduceTasks;
+    std::mutex mtx;
 };
 
 void RunServer() {
